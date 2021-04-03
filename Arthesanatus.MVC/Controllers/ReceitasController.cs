683b451472f29d32e6ed731ffe6471df2e9d7f4f
Joao Galeao <jbgaleao@gmail.com>
2021-04-03 19:43:03 -0300

    Implementação das demais funções - CRUD

---
+++ Arthesanatus.MVC/Controllers/ReceitasController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Arthesanatus.Data.Context;
+using Arthesanatus.Domain.Entities;
+
+namespace Arthesanatus.MVC.Controllers
+{
+    public class ReceitasController : Controller
+    {
+        private ArthesContext db = new ArthesContext();
+
+        // GET: Receitas
+        public ActionResult Index()
+        {
+            var rECEITAS = db.RECEITAS.Include(r => r.Revista);
+            return View(rECEITAS.ToList());
+        }
+
+        // GET: Receitas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Receita receita = db.RECEITAS.Find(id);
+            if (receita == null)
+            {
+                return HttpNotFound();
+            }
+            return View(receita);
+        }
+
+        // GET: Receitas/Create
+        public ActionResult Create()
+        {
+            ViewBag.RevistaId = new SelectList(db.REVISTAS, "RevistaID", "Tema");
+            return View();
+        }
+
+        // POST: Receitas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ReceitaId,Nome,Descricao,RevistaId")] Receita receita)
+        {
+            if (ModelState.IsValid)
+            {
+                db.RECEITAS.Add(receita);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RevistaId = new SelectList(db.REVISTAS, "RevistaID", "Tema", receita.RevistaId);
+            return View(receita);
+        }
+
+        // GET: Receitas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Receita receita = db.RECEITAS.Find(id);
+            if (receita == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RevistaId = new SelectList(db.REVISTAS, "RevistaID", "Tema", receita.RevistaId);
+            return View(receita);
+        }
+
+        // POST: Receitas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ReceitaId,Nome,Descricao,RevistaId")] Receita receita)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(receita).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RevistaId = new SelectList(db.REVISTAS, "RevistaID", "Tema", receita.RevistaId);
+            return View(receita);
+        }
+
+        // GET: Receitas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Receita receita = db.RECEITAS.Find(id);
+            if (receita == null)
+            {
+                return HttpNotFound();
+            }
+            return View(receita);
+        }
+
+        // POST: Receitas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Receita receita = db.RECEITAS.Find(id);
+            db.RECEITAS.Remove(receita);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Arthesanatus.MVC/Controllers/RevistasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Arthesanatus.Data.Context;
+using Arthesanatus.Domain.Entities;
+
+namespace Arthesanatus.MVC.Controllers
+{
+    public class RevistasController : Controller
+    {
+        private ArthesContext db = new ArthesContext();
+
+        // GET: Revistas
+        public ActionResult Index()
+        {
+            return View(db.REVISTAS.ToList());
+        }
+
+        // GET: Revistas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Revista revista = db.REVISTAS.Find(id);
+            if (revista == null)
+            {
+                return HttpNotFound();
+            }
+            return View(revista);
+        }
+
+        // GET: Revistas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Revistas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RevistaID,NumeroEdicao,AnoEdicao,MesEdicao,Tema,Foto")] Revista revista)
+        {
+            if (ModelState.IsValid)
+            {
+                db.REVISTAS.Add(revista);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(revista);
+        }
+
+        // GET: Revistas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Revista revista = db.REVISTAS.Find(id);
+            if (revista == null)
+            {
+                return HttpNotFound();
+            }
+            return View(revista);
+        }
+
+        // POST: Revistas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RevistaID,NumeroEdicao,AnoEdicao,MesEdicao,Tema,Foto")] Revista revista)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(revista).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(revista);
+        }
+
+        // GET: Revistas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Revista revista = db.REVISTAS.Find(id);
+            if (revista == null)
+            {
+                return HttpNotFound();
+            }
+            return View(revista);
+        }
+
+        // POST: Revistas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Revista revista = db.REVISTAS.Find(id);
+            db.REVISTAS.Remove(revista);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
